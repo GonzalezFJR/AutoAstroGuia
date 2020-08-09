9f2d619b635290744efafdb05a323c802a20b8a8
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2020-08-09 23:52:22 +0200

    Adding functions to read from txt catalogs

--- objectFinder.py
+++ objectFinder.py
@@ -1,7 +1,10 @@
-import os, sys, pickle
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+import numpy as np
+
+pathToCatalogs = './catalogs/'
+availableCatalogs = ['messier']
 
 class ObjectPointer:
 
@@ -10,18 +13,44 @@ class ObjectPointer:
     self.SetCoor(lat, lon, alt)
     self.SetTime(day,month,year,h,minut,seg,utcoffset)
     self.verbose = False
-    self.LoadCatalog()
+    self.pathToCatalogs = pathToCatalogs
+    self.availableCatalogs = availableCatalogs
+    self.otherCatalogs = availableCatalogs
+    for c in ['NGC', 'messier', 'IC', 'HD']: 
+      if c in self.otherCatalogs: self.otherCatalogs.pop(self.otherCatalogs.index(c))
+    self.ReadCatalogs()
+
+  def ReadCatalogs(self):
+    ''' Read all catalogs and store as tables '''
+    self.catalogs = {}
+    for cat in self.availableCatalogs:
+      cattxt = self.pathToCatalogs+cat+'.txt'
+      if not os.path.isfile(cattxt):
+        print('WARNING: catlog "%s" not found...'%cattxt)
+        continue
+      tab = np.genfromtxt(cattxt)
+      self.catalogs[cat] = tab
+
+  def FindInCatalog(self, name):
+    ''' Search for object in catalof and returns ra, dec '''
+    catalog = ''
+    ra = ''; dec = ''
+    if   name.startswith('NGC'): catalog = 'NGC'
+    elif name.startswith('IC' ): catalog = 'IC'
+    elif name.startswith('HD' ): catalog = 'HD'
+    elif name.startswith('M'  ): catalog = 'messier'
+    else                       : catalog = self.otherCatalogs
+    if not isinstance(catalog, list): catalog = [catalog]
+    for cat in catalog:
+      if not name.upper() in self.catalogs[cat]: continue
+      ra, dec = tab[np.where(tab==name.upper())[0][0]][1:]
+    if ra=='' or dec == '':
+      print('WARNING: object "%s" not found in any catalog'%name)
+      return None
+    else: return float(ra), float(dec)
 
   def SetVerbose(self, verbose=True):
     self.verbose = verbose
-
-  def LoadCatalog(self, name='.catalog'):
-    ''' Load pickle file with stored coordinates '''
-    self.catalogFileName = name
-    self.catalog = {}
-    if os.path.isfile(name): 
-      with open(name, 'rb') as f: self.catalog = pickle.load(f)
-    print('catalog = ', self.catalog)
 
   def SetCoor(self, lat=0, lon=0, alt=0):
     ''' Set local coordinates '''
@@ -34,15 +63,7 @@ class ObjectPointer:
 
   def GetSkyCoorFromName(self, name):
     ''' Returns sky coordinates for a named object '''
-    name = name.replace(' ', '').upper()
-    if name in self.catalog: 
-      coor  = self.catalog[name]
-    else:
-      coor = SkyCoord.from_name(name)
-      self.catalog[name] = coor
-      with open(self.catalogFileName, 'wb') as f: pickle.dump(self.catalog, f)
-    ra = coor.ra.degree
-    dec = coor.dec.degree
+    coor = SkyCoord.from_name(name)
     if self.verbose: print('%s: [ra, dec] = [%1.3f, %1.3f]'%(name, coor.ra, coor.dec))
     return coor
 
@@ -58,18 +79,17 @@ class ObjectPointer:
 
   def TransformCoordinates(self, alt, azm):
     ''' Transform sky coordinates to alt in [0, 180], azm in [-90, 90] '''
-    if azm>180:
+    if azm>180: 
       azm = azm-180
       alt = 180-alt
     return alt, azm
-
-### Test
+'''
 obj = 'arcturus'
 o = ObjectPointer(43.3, -5.9, 200, 8, 10, 20, 35, 0, 2021, 1)
 M33 = o.GetSkyCoorFromName(obj)
-for obj in ['M33', 'M34']: o.GetSkyCoorFromName(obj)
-#print('%s = '%obj, M33)
-#alt, azm = o.GetLocalSkyCoorFromName(obj)
-#print('alt = ', alt, ', azm = ', azm)
-#alt2, azm2 = o.TransformCoordinates(alt, azm)
-#print('Local coordinates: alt = ', alt2, ', azm = ', azm2)
+print('%s = '%obj, M33)
+alt, azm = o.GetLocalSkyCoorFromName(obj)
+print('alt = ', alt, ', azm = ', azm)
+alt2, azm2 = o.TransformCoordinates(alt, azm)
+print('Local coordinates: alt = ', alt2, ', azm = ', azm2)
+'''
